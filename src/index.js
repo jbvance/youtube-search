a202c6561b14a41b5802b1d226d55b2d33c22613
Jason Vance <jbvance@gmail.com>
2017-12-26 12:08:49 -0600

    add video detail (player) functionality

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
 const API_KEY = 'AIzaSyD8fLlRTTpQ45vLCNBFa_ENk7c-XUOmbmA';
 
 class App extends Component {
@@ -20,6 +22,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]}/>
         <VideoList videos={this.state.videos} />
       </div>);
   }
