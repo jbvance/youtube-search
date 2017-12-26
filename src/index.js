53d7adda8119779fd970c833d4d1e1dfea85f3e7
Jason Vance <jbvance@gmail.com>
2017-12-26 09:32:38 -0600

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const API_KEY = 'AIzaSyD8fLlRTTpQ45vLCNBFa_ENk7c-XUOmbmA';
+
+const App = () => {
+  return <div>Hello, World!</div>;
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
