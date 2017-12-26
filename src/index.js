861e2c1fd9fa715c461c36876993e3004ab6ad2e
Jason Vance <jbvance@gmail.com>
2017-12-26 10:06:19 -0600

    begin search bar component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyD8fLlRTTpQ45vLCNBFa_ENk7c-XUOmbmA';
 
 const App = () => {
-  return <div>Hello, World!</div>;
+  return (
+  <div>
+    <SearchBar />
+  </div>);
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
